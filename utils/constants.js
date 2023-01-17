9aa8e1c7c7cd6043cf55665c9f52cfb7b4019c57
Lance Toledo <50975456+lancetoledo@users.noreply.github.com>
2023-01-17 13:18:39 -0500

    Updated

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,5 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const RPC_ENDPOINT = "https://thrilling-tiniest-log.solana-devnet.discover.quiknode.pro/32db1054321331044575a09be9d349ba91f116ae/";
+export const RPC_ENDPOINT = "https://muddy-aged-panorama.solana-devnet.discover.quiknode.pro/0fe7822c98ade63f96ae1be8e82d17b26d57cacc/";
 
 export const PROGRAM_ID = new PublicKey("5ExEq9ZxGfwSyw5ov4UFK7ws9NpDf2KnUk3wLqfAir4m")
